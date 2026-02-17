a3b0d7adb7f8f8f1091916b21a0e8b855ac6f2f3
R3nc <renielr3n@gmail.com>
2026-02-18 02:27:38 +0800

    ren- nag add ako ng print dito!

--- rv_file.py
+++ rv_file.py
@@ -4,3 +4,5 @@ addris = "tarlak"
 pera = 24.6
 bahay = 2
 print(f"my name is: {name} and my age is {age} i live in: {addris} me mone: {pera} bahay ko {bahay}")
+
+print("ako si ren")#eto yung inadd ko nag lagay ako ng print
